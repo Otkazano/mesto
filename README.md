012210675687615a90113477f65f2b7e83087a88
Vladimir Strugov <Strugov2001@yandex.ru>
2023-06-13 13:59:01 +0300

    добавить версию для телефона и планшета

--- README.md
+++ README.md
@@ -1,17 +1,12 @@
-# Проект: Место
-
-### Обзор
-
-* Figma
-* Картинки
-
-**Figma**
-
-* [Ссылка на макет в Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?node-id=0%3A1)
-
-**Картинки**
-
-Доставать картинки предстоит из Фигмы. Это расхожая практика, поэтому полезно потренироваться.
-Не забудьте [оптимизировать картинки](https://tinypng.com/), чтобы ваш сайт загружался быстрее.
-
-Удачи!
+# Место! 
+[Увидеть проект!](https://otkazano.github.io/mesto/)
+-
+### Технологии в проекте:  
+* продвинутая семантика HTML5
+* CSS
+* JavaScript
+* flexbox / grid
+* адаптивная верстка через media-queries
+* наименование классов и организация файловой структуры Nested
+* подключены шрифты и их сглаживание
+* сайт сверстан по макету из [Figma](https://www.figma.com/file/2cn9N9jSkmxD84oJik7xL7/JavaScript.-Sprint-4?type=design&node-id=0-1&t=ZWidPM0x7cIOVl2W-0)
---
+++ scripts/script.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const editButtonElement = document.querySelector('.profile__edit');
+const closeButtonElemnt = document.querySelector('.popup__btn-close');
+const saveButtonElemnt = document.querySelector('.popup__btn-save');
+const popupElemnt = document.querySelector('.popup');
+
+let nameProfileElement = document.querySelector('.profile__name');
+let aboutProfileElement = document.querySelector('.profile__about');
+
+function openPopup() {
+  popupElemnt.classList.toggle('popup__opened');
+}
+
+function editProfile() {
+  nameProfileElement.textContent = document.querySelector('.popup__area-name').value;
+  aboutProfileElement.textContent = document.querySelector('.popup__area-about').value;
+  popupElemnt.classList.toggle('popup__opened');
+}
+
+editButtonElement.addEventListener('click', openPopup);
+closeButtonElemnt.addEventListener('click', openPopup);
+saveButtonElemnt.addEventListener('click', editProfile);

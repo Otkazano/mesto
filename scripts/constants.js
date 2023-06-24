7a8749dcb9f813a95dba043b4b161199ecc2089c
Vladimir Strugov <Strugov2001@yandex.ru>
2023-06-24 15:59:47 +0300

    feat: add popups and likes

---
+++ scripts/constants.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: 'Металлострой',
+    link: './images/Saint-P.jpg'
+  },
+  {
+    name: 'Озеро Байкал',
+    link: './images/Baikal.jpg'
+  },
+  {
+    name: 'Гамсутль',
+    link: './images/Gamsutl.jpg'
+  },
+  {
+    name: 'Санкт-Петербург',
+    link: './images/Saint-P2.jpg'
+  },
+  {
+    name: 'Москва',
+    link: './images/Moscow.jpg'
+  },
+  {
+    name: 'Архангельск',
+    link: './images/Arkhangelsk.jpg'
+  }
+];
--- scripts/index.js
+++ scripts/index.js
@@ -1,29 +1,141 @@
-'use strict';
+'use strict'
 
-const editButtonElement = document.querySelector('.profile__btn-edit');
-const closeButtonElemnt = document.querySelector('.popup__btn-close');
-const popupElemnt = document.querySelector('.popup');
+// popup for editing the profile
 
-let formElement = document.querySelector('.popup__form')
+const editProfileButtonElement = document.querySelector('.profile__btn-edit')
+const closeProfileButtonElemnt = document.querySelector(
+  '.popup-profile__btn-close'
+)
+const popupProfileElemnt = document.querySelector('.popup-profile')
 
-let nameProfileElement = document.querySelector('.profile__name');
-let aboutProfileElement = document.querySelector('.profile__about');
-let namePopupElement = document.querySelector('.popup__input_el_name');
-let aboutPopupElemnt = document.querySelector('.popup__input_el_about');
+const formProfileElement = document.querySelector('.popup-profile__form')
 
-function openPopup() {
-  popupElemnt.classList.toggle('popup_opened');
-  namePopupElement.value = nameProfileElement.textContent;
-  aboutPopupElemnt.value = aboutProfileElement.textContent;
+const nameProfileElement = document.querySelector('.profile__name')
+const aboutProfileElement = document.querySelector('.profile__about')
+const nameProfilePopupElement = document.querySelector(
+  '.popup-profile__input_el_name'
+)
+const aboutProfilePopupElemnt = document.querySelector(
+  '.popup-profile__input_el_about'
+)
+
+function openAndClosePopupEditProfile () {
+  popupProfileElemnt.classList.toggle('popup_opened')
+  nameProfilePopupElement.value = nameProfileElement.textContent
+  aboutProfilePopupElemnt.value = aboutProfileElement.textContent
 }
 
-function editProfile(e) {
-  e.preventDefault();
-  nameProfileElement.textContent = document.querySelector('.popup__input_el_name').value;
-  aboutProfileElement.textContent = document.querySelector('.popup__input_el_about').value;
-  popupElemnt.classList.toggle('popup_opened');
+function editProfile (e) {
+  e.preventDefault()
+  nameProfileElement.textContent = document.querySelector(
+    '.popup-profile__input_el_name'
+  ).value
+  aboutProfileElement.textContent = document.querySelector(
+    '.popup-profile__input_el_about'
+  ).value
+  popupProfileElemnt.classList.toggle('popup_opened')
 }
 
-editButtonElement.addEventListener('click', openPopup);
-closeButtonElemnt.addEventListener('click', openPopup);
-formElement.addEventListener('submit', editProfile);
+editProfileButtonElement.addEventListener('click', openAndClosePopupEditProfile)
+closeProfileButtonElemnt.addEventListener('click', openAndClosePopupEditProfile)
+formProfileElement.addEventListener('submit', editProfile)
+
+// popup for creating a new image
+
+const newImageButtonElement = document.querySelector('.profile__btn-add')
+const closeNewImageButtonElemnt = document.querySelector(
+  '.popup-newimage__btn-close'
+)
+const popupNewImageElemnt = document.querySelector('.popup-newimage')
+
+const formNewImageElement = document.querySelector('.popup-newimage__form')
+const newImageNamePopupElement = document.querySelector(
+  '.popup-newimage__input_el_name'
+)
+const newImageLinkPopupElemnt = document.querySelector(
+  '.popup-newimage__input_el_link'
+)
+
+function openAndClosePopupNewImage () {
+  popupNewImageElemnt.classList.toggle('popup_opened')
+  newImageNamePopupElement.value = ''
+  newImageLinkPopupElemnt.value = ''
+}
+
+function createNewImage (e) {
+  e.preventDefault()
+  const galleryTemplate = document.querySelector('#template-gallery').content
+  const galleryElement = document.querySelector('.gallery')
+
+  const galleryItemElement = galleryTemplate
+    .querySelector('.gallery__item')
+    .cloneNode(true)
+
+  galleryItemElement.querySelector('.gallery__location').textContent =
+    newImageNamePopupElement.value
+  galleryItemElement.querySelector('.gallery__photo').src =
+    newImageLinkPopupElemnt.value
+  galleryItemElement.querySelector(
+    '.gallery__photo'
+  ).alt = `Достопримечательность из: ${newImageNamePopupElement.value}`
+
+  galleryElement.prepend(galleryItemElement);
+  popupNewImageElemnt.classList.toggle('popup_opened');
+
+  // Likes
+  const likeButtonElement = galleryElement.querySelector('.gallery__btn-like')
+
+  likeButtonElement.addEventListener('click', function (event) {
+    event.target.classList.toggle('gallery__like_active');
+  })
+
+  // Delete cards
+
+  const deleteButtonElement = document.querySelector('.gallery__delete')
+
+  deleteButtonElement.addEventListener('click', function () {
+    const galleryItem = deleteButtonElement.closest('.gallery__item')
+    galleryItem.remove()
+  })
+}
+
+newImageButtonElement.addEventListener('click', openAndClosePopupNewImage)
+closeNewImageButtonElemnt.addEventListener('click', openAndClosePopupNewImage)
+formNewImageElement.addEventListener('submit', createNewImage)
+
+// loading start pack cards
+
+initialCards.forEach(function loadPackCards ({name, link}) {
+  const galleryTemplate = document.querySelector('#template-gallery').content
+  const galleryElement = document.querySelector('.gallery')
+
+  const galleryItemElement = galleryTemplate
+    .querySelector('.gallery__item')
+    .cloneNode(true)
+
+  galleryItemElement.querySelector('.gallery__location').textContent =
+    name
+  galleryItemElement.querySelector('.gallery__photo').src =
+    link
+  galleryItemElement.querySelector(
+    '.gallery__photo'
+  ).alt = `Достопримечательность из: ${name}`
+
+  galleryElement.prepend(galleryItemElement);
+
+  // Likes
+  const likeButtonElement = galleryElement.querySelector('.gallery__btn-like')
+
+  likeButtonElement.addEventListener('click', function (event) {
+    event.target.classList.toggle('gallery__like_active');
+  })
+
+  // Delete cards
+
+  const deleteButtonElement = document.querySelector('.gallery__delete')
+
+  deleteButtonElement.addEventListener('click', function () {
+    const galleryItem = deleteButtonElement.closest('.gallery__item')
+    galleryItem.remove()
+  });
+});

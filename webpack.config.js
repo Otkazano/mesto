ffdddb3a612b81bae3b93e9ac6f4e6a029313c4d
Vladimir Strugov <Strugov2001@yandex.ru>
2023-08-23 10:02:24 +0300

    refactor

--- webpack.config.js
+++ webpack.config.js
@@ -24,9 +24,7 @@ module.exports = {
       use: 'babel-loader',
       exclude: '/node_modules/'
     },
-    // добавили правило для обработки файлов
     {
-      // регулярное выражение, которое ищет все файлы с такими расширениями
       test: /\.(png|svg|jpg|gif|woff(2)?|eot|ttf|otf)$/,
       type: 'asset/resource'
     },
